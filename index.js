9da721ee41f02235bb57a9cb53a1d924b3893a36
Gabriel Cari <g.cari@hotmail.com>
2022-05-20 18:50:54 -0300

    Feat: initials files.

---
+++ index.js
@@ -0,0 +1,13 @@
+import express, {json} from "express";
+import dotenv from "dotenv";
+import chalk from "chalk";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.listen(process.env.PORT, () => 
+    console.log(chalk.bold.green(`Server online on port ${process.env.PORT}!`))
+);
