886013f92785a34a30525ee0b1203ce42965b190
Allan Ornel <allanornel@gmail.com>
2022-05-20 21:35:40 -0300

    fix: fix on questionController, change var questions

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,16 +1,20 @@
 import db from "../config/db.js";
 
-export async function getQuestions(req, res){
-    const { dificulty } = req.query;
-    
-    try{
-        if(dificulty){
-            const questions = await db.collection("questions").find({dificulty}).toArray();
-        }else{
-            const questions = await db.collection("questions").find({}).toArray(); 
-        }
-        return res.status(200).send(questions);
-    }catch(e){
-        return res.status(500).send("Error on GET /questions", e);
+export async function getQuestions(req, res) {
+  const { dificulty } = req.query;
+
+  try {
+    let questions;
+    if (dificulty) {
+      questions = await db
+        .collection("questions")
+        .find({ dificulty })
+        .toArray();
+    } else {
+      questions = await db.collection("questions").find({}).toArray();
     }
+    return res.status(200).send(questions);
+  } catch (e) {
+    return res.status(500).send("Error on GET /questions", e);
+  }
 }
